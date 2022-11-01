f098242ca2089a08e878e22291fa11f1bedd6ca4
pandapythoner <stepanov.aa@students.sch2.ru>
2022-11-01 14:31:44 +0300

    Added home_page

--- zavalinka/zavalinka/settings.py
+++ zavalinka/zavalinka/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zavalinka_game.apps.ZavalinkaGameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
